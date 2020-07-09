ecf6696a3fde15340377a081e7b49dbca87a36f2
Kyle Halleman <kylehalleman@users.noreply.github.com>
2020-07-08 22:43:09 -0500

    fix: unused modules

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-const { JSDOM } = require("jsdom");
-const fetch = require("node-fetch");
 const fs = require("fs");
 const path = require("path");
 const { promisify } = require("util");
@@ -17,18 +15,15 @@ nunjucks.configure("src", {
 });
 
 const dataPath = path.resolve(process.cwd(), "node_modules/quickref/_data");
+const buildDir = path.resolve(process.cwd(), "build");
 
 readFile(path.resolve(dataPath, "wcag21.json"), "utf-8").then((json) => {
   const { principles } = JSON.parse(json);
 
   const rendered = nunjucks.render("index.html", { principles });
+  const minified = minify(rendered, { collapseWhitespace: true });
 
-  mkdir(path.resolve(process.cwd(), "build"), { recursive: true }).then(() =>
-    writeFile(
-      path.resolve(process.cwd(), "build/index.html"),
-      minify(rendered, { collapseWhitespace: true })
-    )
+  mkdir(buildDir, { recursive: true }).then(() =>
+    writeFile(path.resolve(buildDir, "index.html"), minified)
   );
-
-  console.log(principles);
 });
